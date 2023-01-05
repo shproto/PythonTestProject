2d58552b3525276a2566faca69ccf74731d92d93
AlexKalitin <a.s.kalitin@gmail.com>
2023-01-05 14:54:17 +0300

    First dict lesson

--- main.py
+++ main.py
@@ -1,22 +1,48 @@
-import requests as rq
-from requests_ntlm import HttpNtlmAuth
-resp = rq.get('https://api.github.com')
-print(resp.headers['server'])
-# This is a sample Python script.
+import copy #for deep copy
+dict1 = {
+    "ip": "127.0.0.1",
+    ("port"): {         #строго говоря не является кортежем, но обычной строкой. для кортежа надо запятую (4000, )
+        "1":4000,
+        "2":5000}
+}
+dict2 = dict(he=36, she=35, little1=7, little2=4)
+print("dict1: ", dict1['port']['2'])
+print("dict2: ", dict2)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#annotations
+def get_dict() -> dict:
+    return dict(one=1, two=[2, 22, 222], three=3, four=4)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+a: dict = get_dict()
+print(a.get("four", "есть в наличии"))
+print(a.get("five", "nothing"))
 
 
+# add key to dict
+a["five"]=5
+a.update({"six":6, "seven": 7})
+print(a)
+
+#del key from dict
+del a["seven"]
+print(r'"a after del a['seven']: ", a)
+print("default a.pop message when nothing found: ", a.pop("seven", "nothing found"))
+
+#check key in dict
+print("seven" in a)
+print("six" in a)
+
+#copy a dict
+b = a # this is a ling
+print(f'a id: {id(a)}, b id: {id(b)}') # a and b are the same. The same cell in the memory
+c = a.copy() #this is a  copy of a
+#this is the same cell:
+c['two'] = 22
+print(id(a['two']), "a: ", a)
+print(id(c['two']), "c: ", c)
+d = copy.deepcopy(a) #deep copy
+d['two'] = 222
+print(id(a['two']),"a: ", a)
+print(id(d['two']),"d: ", d)
+
+print('stop row')
